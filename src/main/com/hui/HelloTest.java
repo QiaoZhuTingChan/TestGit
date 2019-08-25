f5aacc3ee1eedb41e43532b325300ac3d9fc8b43
hui <1779788260@qq.com>
2019-08-25 16:45:56 +0800

    first commit

---
+++ src/main/com/hui/HelloTest.java
@@ -0,0 +1,16 @@
+package com.hui;
+
+import org.junit.Test;
+
+/**
+ * @author hui
+ * @16:38 2019/8/25
+ */
+public class HelloTest {
+
+    @Test
+    public void test1(){
+        String value1 = "哈哈哈";
+        System.out.println(value1);
+    }
+}
